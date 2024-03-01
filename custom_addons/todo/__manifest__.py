3405d5759afaccd55a97c46b57530147fac9b148
elrmam <greatdream0000@gmail.com>
2024-03-02 00:35:00 +0200

    todo

---
+++ custom_addons/todo/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Todo App",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Ahmed Reda",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['base', ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menus.xml',
+        'views/ticket.xml',
+    ],
+}
---
+++ custom_addons/todo/models/ticket.py
@@ -0,0 +1,16 @@
+from odoo import fields, models, api
+
+class Ticket(models.Model):
+    _name = 'todo.ticket'
+    _description = 'Todo'
+
+    name = fields.Char()
+    number = fields.Integer()
+    tag = fields.Char()
+    state = fields.Selection([
+        ('new', 'New'),
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    ])
+    file = fields.Binary()
+    description = fields.Char()
